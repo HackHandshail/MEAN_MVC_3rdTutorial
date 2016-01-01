38cf5d22e010188be4f87416c8c5626e18acc606
Shailesh Kumar <shailesh.kumar.786@gmail.com>
2016-01-01 20:55:29 +0530

    added todo files

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 
+
 //node_modules/mongodb/node_modules/bson/ext/index.js  change line no. 10 to   bson = require('../build/browser_build/bson');  
+
+
+svn add --force * --auto-props --parents --depth infinity -q
--- public/application.js
+++ public/application.js
@@ -1,5 +1,5 @@
 var appName = 'mean';
-var app = angular.module(appName, ['ngRoute','example']);
+var app = angular.module(appName, ['ngRoute','example', 'users']);
 
 angular.element(document).ready(function() {
 		angular.bootstrap(document, [appName]);
