73a7f7fcaaa4268019fb44ab1746751a0a55cb07
Shailesh Kumar <shailesh.kumar.786@gmail.com>
2016-01-01 19:50:42 +0530

    updating example module

--- public/application.js
+++ public/application.js
@@ -1,7 +1,13 @@
 var appName = 'mean';
-var app = angular.module(appName, ['example']);
+var app = angular.module(appName, ['ngRoute','example']);
 
 angular.element(document).ready(function() {
-    angular.bootstrap(document, [appName]);
-});
+		angular.bootstrap(document, [appName]);
+		});
 
+
+//for single page routing
+app.config(['$locationProvider', function($locationProvider) {
+		$locationProvider.hashPrefix('!');
+		}
+		]);
